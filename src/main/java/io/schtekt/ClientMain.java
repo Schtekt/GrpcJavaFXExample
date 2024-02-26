bc0edc25b7c0abf891a6a77080a96acca6bf3fc8
Tim Johansson <totimom@live.se>
2024-02-26 10:42:22 +0100

    Use Maven assembly plugin to produce jars
    Produce jars with all depndencies through Maven Assembly plugin

---
+++ src/main/java/io/schtekt/ClientMain.java
@@ -0,0 +1,7 @@
+package io.schtekt;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        ClientApp.main(args);
+    } 
+}
---
+++ src/main/java/io/schtekt/ServerMain.java
@@ -0,0 +1,7 @@
+package io.schtekt;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        ServerApp.main(args);
+    } 
+}
